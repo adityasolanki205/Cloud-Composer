f98c859edd06b79b18e4ae7f1405a3efaddb2735
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2021-08-17 22:14:15 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,8 +8,6 @@ job_name ='batch-pipeline'
 # ValueProviders like source table name and sink table name
 input_data ='gs://{bucket}/german.data'.format(bucket=template_bucket)
 #Template's GCS path
-template_gcs_path='gs://{bucket}/template/batch-pipeline-template'.format(bucket=template_bucket)
+template_gcs_path='gs://{bucket}/Template/batch-pipeline-template'.format(bucket=template_bucket)
 #Temprory path location
 temp_location='gs://{bucket}/Temp'.format(bucket=template_bucket)
-#input param name, depends on what you have defined in your dataflow pipeline
-input_param_name = input_data
