58dd9dad075558707afb803f32085ec4461ae7ab
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2021-08-17 22:14:50 +0530

    Update daily_sync_dag.py

--- daily_sync_dag.py
+++ daily_sync_dag.py
@@ -1,8 +1,10 @@
 import airflow
 from airflow import DAG
+from airflow import models
 from airflow.operators.python_operator import PythonOperator
-from trigger_job_util import TriggerJobUtil
 from dag_arguments import DAGArgs
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
 import Config
 
 def daily_sync_etl():
@@ -11,22 +13,28 @@ def daily_sync_etl():
         temp_location = Config.temp_location
         zone = Config.zone
         input_data = Config.input_data
-        input_param_name = Config.input_param_name
+        #input_param_name = Config.input_param_name
         gcs_path = Config.template_gcs_path
-        body = DAGArgs().get_daily_trigger_dataflow_body(job_name,temp_location,zone,input_data,input_param_name)
-        TriggerJobUtil.trigger_job(gcs_path, body)
+        body = DAGArgs.get_daily_trigger_dataflow_body(job_name,temp_location,zone,input_data)
+        credentials = GoogleCredentials.get_application_default()
+        service = build('dataflow', 'v1b3', credentials=credentials,cache_discovery=False)
+        request = service.projects().templates().launch(projectId=Config.project_name, gcsPath=Config.template_gcs_path, body=body)
+        response = request.execute()
+        print("-----execute function have been called-------")
+        print(response)
+        #TriggerJobUtil.trigger_job(gcs_path, body)
     except Exception as ex:
         print(['Exception', ex])
         
 try:
-    with DAG(
-        'GCS_to_Bigquery_dag',
-        default_args=DAGArgs().default_args,
-        description='DAG for Hourly sync to cloud sql',
-        max_active_runs=1,
-        concurrency=4,
-        catchup = False,
-        schedule_interval='@hourly',
+    with models.DAG(
+            'GCS_to_Bigquery_dag',
+            default_args=DAGArgs().default_args,
+            description='DAG for Hourly sync to cloud sql',
+            max_active_runs=1,
+            concurrency=4,
+            catchup = False,
+            schedule_interval='@hourly'
         ) as dag:
             task1 = PythonOperator(
                     task_id='daily-sync-etl',
